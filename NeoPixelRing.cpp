c16f675a6b2d03d08dcbb820ad6eadb0c9a74628
Paul Langemeijer <PaulLangemeijer@hotmail.com>
2019-06-19 21:26:30 +0200

    change pin led ring

--- NeoPixelRing.cpp
+++ NeoPixelRing.cpp
@@ -1,7 +1,8 @@
 //  Copyright © 2019 Paul Langemeijer. All rights reserved.
 #include "NeoPixelRing.h"
 
-const int pinLedRing = 17; // PC3
+//const int pinLedRing = 17; // PC3
+const int pinLedRing = 16; // PC2
 const int NumberOfLeds = 24;
 const int Max5Bits = 31;
 
